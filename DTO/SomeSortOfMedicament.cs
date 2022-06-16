f81ace5898a4b84fc939fdf9cd5e99888c1c50ad
Tomasz Wierzbowski <tomaswierzbowski@gmail.com>
2022-06-16 20:56:07 +0200

    small amendment

--- DTO/SomeSortOfMedicament.cs
+++ DTO/SomeSortOfMedicament.cs
@@ -2,8 +2,8 @@
 {
     public class SomeSortOfMedicament
     {
-        public string Name;
-        public string Description;
-        public string Type;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
     }
 }
